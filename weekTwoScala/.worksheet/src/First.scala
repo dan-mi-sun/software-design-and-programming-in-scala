9208672a14714d4cf3ce5e952d6fc70fc6311ba0
Keith Mannock <keith@dcs.bbk.ac.uk>
2015-01-25 20:16:50 +0000

    tidy

--- weekTwoScala/.worksheet/src/First.scala
+++
@@ -1,5 +0,0 @@
-object First {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(58); 
-  println("Welcome to the Scala worksheet");$skip(7); val res$0 = 
-	3 + 4;System.out.println("""res0: Int(7) = """ + $show(res$0));$skip(25); val res$1 = 
-	3 min 4 min 5 min 2 5 6;System.out.println("""res1: Int = """ + $show(res$1))}
-}
--- weekTwoScala/src/myFirst/Person.scala
+++
@@ -1,7 +0,0 @@
-package myFirst
-
-class Person(first: String, last: String) {
-  val firstName = first
-  val lastName = last
-  def name = firstName + " " + lastName
-}
