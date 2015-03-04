cfd5040db96171a3944de761e57c6f5dd96971c4
Keith Mannock <keith@dcs.bbk.ac.uk>
2015-03-04 17:00:23 +0000

    RayTracer = cw3

---
+++ DAG/src/Iterat.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Iterat {
+    public static void main(String[] args) {
+        List<String> ls = Arrays.asList("a","b","c");
+
+        Iterat il = ls.iterator();
+
+//        for(String s: ls){
+//            if (s.equals("b"))
+//                ls.remove(s);
+//            System.out.println(s);
+//        }
+//        System.out.println(ls);
+
+    }
+}
---
+++ DAG/src/Singleton.java
@@ -0,0 +1,5 @@
+/**
+ * Created by keith on 03/03/2015.
+ */
+public class Singleton {
+}
